557e0f73fb4de3bbe5a9a2d105af97de96427ef0
Alex Panko <alexander.panko@gmail.com>
2022-06-22 14:22:28 +0200

    Update template image path and delete from server

--- controllers/overlays.js
+++ controllers/overlays.js
@@ -5,19 +5,24 @@ const asyncHandler = require('../middleware/async');
 const path = require('path');
 const Overlay = require('../models/Overlay');
 
+// Utils to remove file from server
+const fs = require('fs');
+const util = require('util');
+const unlinkFile = util.promisify(fs.unlink);
+
 // Require the Cloudinary library and configuration
 const cloudinary = require('../config/cloudinary');
 
 // @desc     Get all overlays
 // @route    GET /api/v1/overlays
-// @access   Public
+// @access   Private
 exports.getOverlays = asyncHandler(async (req, res, next) => {
   res.status(200).json(res.advancedResults);
 });
 
 // @desc     Get single overlays
 // @route    GET /api/v1/overlays/:id
-// @access   Public
+// @access   Private
 exports.getOverlay = asyncHandler(async (req, res, next) => {
   const overlay = await Overlay.findById(req.params.id);
 
@@ -85,7 +90,7 @@ exports.overlayImageUpload = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse(`Please upload a file`, 400));
   }
 
-  const file = req.files.file
+  const file = req.files.file;
 
   // Make sure the image is a photo
   if (!file.mimetype.startsWith('image')) {
@@ -123,7 +128,7 @@ exports.overlayImageUpload = asyncHandler(async (req, res, next) => {
     `./${process.env.OVERLAY_IMAGE_UPLOAD_PATH}/${file.name}`,
     {
       public_id: path.parse(file.name).name,
-      folder: 'templates'
+      folder: 'templates',
     },
     function (error, result) {
       res.status(200).json({
@@ -133,7 +138,14 @@ exports.overlayImageUpload = asyncHandler(async (req, res, next) => {
     }
   );
 
-  // Remove file from server
+  // Update overlay image path in database
+  await Overlay.findByIdAndUpdate(req.params.id, {
+    new: true,
+    runValidators: true,
+    image: `${process.env.CLOUDINARY_IMAGE_UPLOAD_PATH}/${file.name}`,
+  });
 
-
+  //Remove file from server
+  const imagePath = `${process.env.OVERLAY_IMAGE_UPLOAD_PATH}/${file.name}`;
+  await unlinkFile(imagePath);
 });
